e2f506163a8f0e6efd5c2218e4601ea746f53435
Ivan Sakharov <vingving648@gmail.com>
2024-10-02 22:40:44 +0300

    add `type_emoji`, do `delete_quest`, add check quest name long

--- src/handlers/quests.py
+++ src/handlers/quests.py
@@ -1,4 +1,7 @@
+import aiogram
+
 from . import *
+from utils.exceptions import *
 
 
 router = Router()
@@ -28,28 +31,43 @@ async def see_one_quest(query: types.CallbackQuery, callback_data: QuestAction):
 	q_type = getattr(quest, 'type')
 
 	if q_type == 'easy':
-		type = '☀'
+		type_emoji = '☀'
+		type_name = 'Easy'
 	elif q_type == 'middle':
-		type = '👾'
+		type_emoji = '👾'
+		type_name = 'Middle'
 	elif q_type == 'boss':
-		type = '👹'
+		type_emoji = '👹'
+		type_name = 'Boss'
 
 	await query.message.answer(
 		text=f'''
-📌 Quest: {getattr(quest, 'name')} 
-Type: {type}
+<b>📌 Quest</b>
+Name: {getattr(quest, 'name')} {type_emoji}
+Type: {type_name}
 
 Benefits: {getattr(quest, 'benefits')}
-'''
+''',    reply_markup=under_quest_kb.as_markup(),
+		parse_mode=ParseMode.HTML,
 	)
 
 
-##########################################################################
+#########################################################################
+# Delete a quest
+@router.callback_query(QuestAction.filter(F.action == 'delete_quest'))
+async def delete_quest(query: types.CallbackQuery, callback_data: QuestAction):
+	await QuestOrm().delete(quest_id=callback_data.quest_id)
+	await query.answer('✅ Successfully deleted')
+	await query.message.delete()
 
+
+##########################################################################
+# Adding a quest
 class AddingQuest(StatesGroup):
 	"""
 	Class for processing adding quest
 	"""
+	start = State()
 	to_get_name = State()
 	to_get_type = State()
 	to_get_benefits = State()
@@ -58,14 +76,14 @@ class AddingQuest(StatesGroup):
 # -------------------------------------------------------------------------
 # Add quest
 @router.callback_query(QuestAction.filter(F.action == 'add_quest'))
-async def add_quest(query: types.CallbackQuery, callback_data: QuestAction, state: FSMContext):
+async def add_quest(
+		query: types.CallbackQuery,
+		callback_data: QuestAction,
+		state: FSMContext,
+	):
 	await state.clear()
-
-	location_id = callback_data.location_id
-	await state.update_data(location_id=location_id)
-
-	await query.message.answer(
-		text='''
+	await state.set_state(AddingQuest.start)
+	text = '''
 Let`s go!
 
 The first field is name.
@@ -77,14 +95,28 @@ You can also call it game words, let's say "Preparing for battle"
 
 So, type your quest name:
 '''
+	location_id = callback_data.location_id
+	await state.update_data(location_id=location_id)
+	await query.message.answer(
+		text=text
 	)
 	await state.set_state(AddingQuest.to_get_name)
 
 
 @router.message(F.text, AddingQuest.to_get_name)
 async def get_name(message: types.Message, state: FSMContext):
-	name = message.text
-	await state.update_data(name=name)
+	try:
+		name = message.text
+		await state.update_data(name=name)
+
+		if len(name) >= 50:
+			raise QuestNameIsLong
+
+	except QuestNameIsLong:
+		await message.answer(
+			text='Sorry, the name is too long. Try again with Map button'
+		)
+		return
 
 	types_kb = InlineKeyboardBuilder()
 	types_kb.row(
@@ -111,31 +143,31 @@ async def get_name(message: types.Message, state: FSMContext):
 The second field is the type.
 The type is the degree of difficulty of the quest.
 
-☀️A simple type of task in real life is a habit. 
-That is, if you don't do it, you won't get hurt, 
-but the reward will be small. In the game: 
-A Easy mob, the reward is small, but regular 
-execution strengthens the character.
-Example: 10 push-ups, clear-desk
+☀ The easy quest in the game: a Easy mob, 
+the reward is small, but regular execution 
+strengthens the character. That is, if you don't do 
+it, you won't get hurt, but the reward will be small. 
+Example: <code>10 push-ups</code>, <code>clear desk</code>
 
 👾 The Middle quest is the Middle mob in the game world. 
 The damage will already be serious, but the reward will 
 also be more valuable.
-Example: learn 10 foreign words, 1 hr studying
+Example: <code>learn 10 foreign words</code>, <code>1 hr studying</code>
 
 👺 The Boss quest - maximum damage and maximum awards
 In real life: A huge goal that requires long preparation 
 and great willpower, but if you do it, it's a huge breakthrough.
-Example: finish a project, win the marathon, complete 1-st chapter of course
+Example: <code>finish a project</code>, <code>win the marathon</code>, <code>complete 1-st chapter of course</code>
 
 So, select the type:
-''',    reply_markup=types_kb.as_markup()
-	)
+''',    reply_markup=types_kb.as_markup(),
+		parse_mode=ParseMode.HTML
+		)
 	await state.set_state(AddingQuest.to_get_type)
 
 
 @router.callback_query(F.data.startswith('type_is_'), AddingQuest.to_get_type)
-async def get_type(query: types.CallbackQuery, callback_data: str, state: FSMContext):
+async def get_type(query: types.CallbackQuery, state: FSMContext):
 	type = query.data.split('_')[-1]
 	await state.update_data(type=type)
 
@@ -143,8 +175,9 @@ async def get_type(query: types.CallbackQuery, callback_data: str, state: FSMCon
 		text='''
 The last field is the benefits.
 
-💡Benefits are the results from the quest, 
+💡 Benefits are the results from the quest, 
 i.e. what you get after completing it. 
+
 For example, if the task “Project” - it is 
 quite possible to add benefits as coins 
 (that is, the game character will get coins, 
@@ -157,22 +190,64 @@ So, type the benefits:
 
 
 @router.callback_query(F.data.startswith('benefits_'), AddingQuest.to_get_benefits)
-async def get_benefits(query: types.CallbackQuery, callback_data: str, state: FSMContext):
-	benefits = callback_data.split('_')[-1]
+async def get_benefits(query: types.CallbackQuery, state: FSMContext, bot: aiogram.Bot):
+	async def test_display_quest(query: types.CallbackQuery, q_name: str) -> dict:
+		"""
+		Test to display the task.
+
+		The user can write a long name and telegram will not be able to send the keyboard
+		"""
+		test_kb = InlineKeyboardBuilder()
+		test_kb.row(
+			types.InlineKeyboardButton(
+				text=f'{q_name} 👾',
+				callback_data=f'test_{q_name}'
+			)
+		)
+		try:
+			test_msg = await query.message.answer(
+				text=f'Quest test {q_name} 👾',
+				reply_markup=test_kb.as_markup()
+			)
+			await test_msg.delete()
+			return {
+				'result': True,
+				'error': None
+			}
+		except Exception as e:
+			return {
+				'result': True,
+				'error': str(e)
+			}
+
+	benefits = query.data.split('_')[-1]
 	await state.update_data(benefits=benefits)
-
 	data = await state.get_data()
-	await QuestOrm().add(
-		user_id=query.from_user.id,
-		name=data['name'],
-		location_id=data['location_id'],
-		type=data['type'],
-		benefits=data['benefits'],
-	)
 
-	await query.message.answer(
-		text='✅ The quest successfully added!'
-	)
-	await state.clear()
-
-
+	res = await test_display_quest(query, data['name'])
+	if res:
+		await QuestOrm().add(
+			user_id=query.from_user.id,
+			name=data['name'],
+			location_id=data['location_id'],
+			type=data['type'],
+			benefits=data['benefits'],
+		)
+		await bot.delete_messages(
+			chat_id=query.message.chat.id,
+			message_ids=[
+				query.message.message_id,
+				query.message.message_id - 1,
+				query.message.message_id - 2,
+				query.message.message_id - 3,
+			]
+		)
+		await query.message.answer(
+			text='✅ The quest successfully added!'
+		)
+		await state.clear()
+	else:
+		await query.message.answer(
+			text=f'😥 Sorry, the quest not added.\nError: {res['error']}'
+		)
+		await state.clear()
